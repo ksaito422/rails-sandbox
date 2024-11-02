26ad9f7c6b89cd733fffb418ddf0b909187a6eb2
ksaito <kitsit.39@gmail.com>
2024-11-02 10:40:42 +0900

    sidekiq: 環境構築

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  mount Sidekiq::Web => "/sidekiq"
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
