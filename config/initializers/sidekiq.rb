3d0f92fa7a73f3c706fd2aa0dea4335653bdb7dd
ksaito <kitsit.39@gmail.com>
2024-11-05 08:47:20 +0900

    sidekiq: ログレベルの設定

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,6 +5,12 @@ redis_url = { url: REDIS_URL }
 
 Sidekiq.configure_server do |config|
   config.redis = redis_url
+  config.death_handlers << ->(job, ex) do
+    puts "Uh oh, #{job['class']} #{job["jid"]} just died with error #{ex.message}."
+  end
+  config.logger = Sidekiq::Logger.new($stdout)
+  config.logger.level = Logger::INFO
+  config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
 end
 
 Sidekiq.configure_client do |config|
