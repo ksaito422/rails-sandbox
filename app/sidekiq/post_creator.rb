c8da294c28c69e935ef8350e87665f4edcb65d11
ksaito <kitsit.39@gmail.com>
2025-01-08 08:51:16 +0900

    sidekiq:

---
+++ app/sidekiq/post_creator.rb
@@ -0,0 +1,23 @@
+class PostCreator
+  include Sidekiq::IterableJob
+
+  def build_enumerator(start_at, count, **kwargs)
+    @start_at = start_at
+    @count = count
+    logger.info("Creating posts for #{start_at}")
+    array_enumerator((start_at...(start_at + count)).to_a, **kwargs)
+  end
+
+  def each_iteration(pid, *_unused_args)
+    sleep 5.minutes
+    Post.create(title: "Post #{pid}", body: "This is post #{pid}")
+  end
+
+  def on_start
+    logger.info("Starting to create posts")
+  end
+
+  def on_complete
+    logger.info("Finished creating posts")
+  end
+end
